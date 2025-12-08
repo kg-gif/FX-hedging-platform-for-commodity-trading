187c477d970d3bd3f3c306dface3e6e0ef7b9cd5
kg-gif <kg@sumnohow.com>
2025-12-08 21:34:17 +0100

    Fix vite output directory

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -14,6 +15,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist'
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true
   }
 });
