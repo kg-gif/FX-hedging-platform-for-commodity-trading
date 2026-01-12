a7fb6d343c31ad7177fa6b75af338c85213dc3de
kg-gif <kg@sumnohow.com>
2026-01-12 22:34:46 +0100

    Fix import path in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Dashboard from './components/Dashboard'
+import Dashboard from './components/Dashboard.jsx'
 import HedgingRecommendations from './components/HedgingRecommendations'
 import ScenarioAnalysis from './components/ScenarioAnalysis'
 import HedgeTracker from './components/HedgeTracker'
